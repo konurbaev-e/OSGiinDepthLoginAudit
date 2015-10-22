7c4bc5a2584fbcf39308b51ee3beb3ab7fe2d74a
konurbaev-e <konurbaev-e@gaz-is.ru>
2015-10-22 19:13:31 +0300

    LoginEventConstants

---
+++ loginaudit-subscriber/src/main/java/org/konurbaev/loginaudit/subscriber/LoginEventConstants.java
@@ -0,0 +1,11 @@
+package org.konurbaev.loginaudit.subscriber;
+
+public interface LoginEventConstants {
+
+    final static String TOPIC_NAME = "manning/osgi/login";
+
+    final static String USERID = "userid";
+
+    final static String TIMESTAMP = "timestamp";
+
+}
